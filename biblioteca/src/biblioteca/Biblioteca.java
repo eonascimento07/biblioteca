f8884e9731cd73667a7027e79322bd31af972bae
Leandro Tavares <Leandro Tavares@Tavares>
2024-03-15 17:54:28 -0300

    Projeto Completo Biblioteca

--- biblioteca/src/biblioteca/Biblioteca.java
+++ biblioteca/src/biblioteca/Biblioteca.java
@@ -15,35 +15,47 @@ public class Biblioteca {
     }
 
     public void emprestarLivro(String titulo) {
+    	boolean encontrado = false;
         for (Livro livro : livros) {
-            if (livro.getTitulo().equals(titulo) == livro.isDisponibilidade()) {
-                livro.setDisponibilidade(false);
-                System.out.println("Livro emprestado: " + livro.getTitulo());
-                return;
+            if (livro.getTitulo().equals(titulo)) {
+                encontrado = true;
+                if (livro.isDisponibilidade()) {
+                    livro.setDisponibilidade(false);
+                    System.out.println("Livro emprestado: " + livro.getTitulo() +", autor: "+ livro.getAutor());
+                } else{
+                    System.out.println("Livro já está emprestado.");
+                }
             }
         }
-        System.out.println("Livro não disponível para empréstimo ou não encontrado.");
+        if (!encontrado) {
+            System.out.println("Livro não encontrado.");
+        }
     }
 
     public void devolverLivro(String titulo) {
+    	boolean encontrado1 = false;
         for (Livro livro : livros) {
-            if (livro.getTitulo().equals(titulo) == !livro.isDisponibilidade()) {
-                livro.setDisponibilidade(true);
-                System.out.println("Livro devolvido: " + livro.getTitulo());
-                return;
+            if (livro.getTitulo().equals(titulo)) {
+                encontrado1 = true;
+                if (!livro.isDisponibilidade()) {
+                    livro.setDisponibilidade(true);
+                    System.out.println("Livro devolvido: " + livro.getTitulo() + "autor: " + livro.getAutor());
+                } else {
+                    System.out.println("Livro não disponivel para devolução.");
+                }
             }
         }
-        System.out.println("Livro não encontrado ou já está disponível.");
+        if (!encontrado1) {
+            System.out.println("Livro não encontrado.");
+        }
+    }
+    public void consultarLivrosDisponiveis() {
+        System.out.println("Todos os livros na biblioteca:");
+        for (Livro livro : livros) {
+            System.out.println("título: " + livro.getTitulo() + " - autor: " + livro.getAutor() + " - " + (livro.isDisponibilidade() ? "Disponível" : "Indisponível"));
+        }
     }
 
-    public void consultarLivrosDisponiveis() {
-        System.out.println("Livros disponíveis na biblioteca:");
-        for (Livro livro : livros) {
-            if (livro.isDisponibilidade()) {
-                System.out.println("- " + livro.getTitulo());
-            }
-        }
-    }
 
 	public List<Livro> getLivros() {
 		return livros;
--- biblioteca/src/biblioteca/Main.java
+++ biblioteca/src/biblioteca/Main.java
@@ -13,49 +13,59 @@ public class Main {
         int opcao;
         do {
             System.out.println("\n--- Menu ---");
-            System.out.println("1. Emprestar livro");
+            System.out.println("1. Adicionar livros");
             System.out.println("2. Devolver livro");
             System.out.println("3. Consultar livros disponíveis");
-            System.out.println("4. Adicionar livros");
+            System.out.println("4. Emprestar livro");
             System.out.println("5. Sair");
             System.out.println("Escolha uma opção: ");
             opcao = scanner.nextInt();
             scanner.nextLine();
             
             switch (opcao) {
-                case 1:
-                    System.out.println("Digite o nome do livro que deseja emprestar ");
-                    String emprestar = scanner.nextLine();
-                    biblioteca.emprestarLivro(emprestar);
-                    break;
+            case 1:
+                System.out.println("Digite o nome do livro que deseja adicionar:");
+                String addTitulo = scanner.nextLine();
+                System.out.println("Digite o nome do autor:");
+                String addAutor = scanner.nextLine();
+                boolean livroExistente = false;
+                for (Livro livro : biblioteca.getLivros()) {
+                    if (livro.getTitulo().equals(addTitulo)) {
+                        livroExistente = true;
+                        break;
+                    }
+                }
+                if (livroExistente) {
+                    System.out.println("Não é possível adicionar o mesmo livro novamente.");
+                } else {
+                    biblioteca.adicionarLivro(new Livro(addTitulo, addAutor, true));
+                    System.out.println("Livro adicionado com sucesso!");
+                }
+                break;
+
                 case 2:
-                    System.out.println("Digite o nome do livro que deseja devolver");
+                    System.out.println("Digite o nome do livro que deseja devolver: ");
                     String devolver = scanner.nextLine();
-                    biblioteca.emprestarLivro(devolver);
+                    biblioteca.devolverLivro(devolver);
                     break;
                 case 3:
                     biblioteca.consultarLivrosDisponiveis();
                     break;
                 case 4:
-                    System.out.println("Digite o nome do livro que deseja adicionar:");
-                    String addTitulo = scanner.nextLine();
-                    System.out.println("Digite o nome do autor:");
-                    String addAutor = scanner.nextLine();
-               
-                    boolean disponibilidade = true;
-                    biblioteca.adicionarLivro(new Livro(addTitulo, addAutor, disponibilidade));
-                    System.out.println("Livro adicionado com sucesso!");
+                    System.out.println("Digite o nome do livro que deseja emprestar: ");
+                    String emprestar = scanner.nextLine();
+                    biblioteca.emprestarLivro(emprestar);
                     break;
                 case 5:
                 	sair = true;
                 	break;
                 	
-               default: System.out.println("Opção inválida"); 
+               default: System.out.println("Opção inválida."); 
                Thread.sleep(1500);
                 
             }
         } while (!sair);
-        System.out.println("Programa encerrado.");
+        System.out.println("Encerrando programa...");
         Thread.sleep(2000);
         clearScreen();
     }
